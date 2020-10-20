455294689157ba854661886c74637ce6be085474
pinkswall <tmddn741741@gmail.com>
2020-10-20 17:06:49 +0900

    Skb 채널 파서 구현
    SKB에서 제공하는 epg 목록 파싱

---
+++ DumpChannels/DumpChannelsFromSkb.py
@@ -0,0 +1,42 @@
+import json
+import requests
+
+def DumpChannelsFromSkb():
+  """
+  SKB에서 제공하는 EPG 목록을 파싱합니다. \n
+  @return [
+    {
+      'SKB Name': '채널이름', 
+      'SKBCh': 채널번호,
+      'Source': 'SKB',
+      'ServiceId': '서비스ID'
+    }
+  ] \n
+  @request_count 1
+  """
+  UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0'
+  req = requests.get("https://m.skbroadband.com/content/realtime/Realtime_List_Ajax.do", headers={'User-Agent': UA})
+  print('Status Code: ', req.status_code)
+
+  channels = json.loads(req.text)
+  result = []
+
+  for channel in channels:
+    if channel['depth'] == '1':
+      continue
+    ch_name = channel['m_name']
+    ch_no = channel['ch_no']
+    ch_id = channel['c_menu']
+    result.append({
+      'SKB Name': ch_name,
+      'SKBCh': int(ch_no),
+      'Source': 'SKB',
+      'ServiceId': ch_id
+    })
+  
+  return result
+  
+
+
+
+print(DumpChannelsFromSkb())
